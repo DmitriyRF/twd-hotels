5ecbd27704d8e6691a481329159e9ac5bd51f495
DmitriyRF <dmitriy_r_f@mail.ru>
2017-03-27 15:17:54 +0300

    first commit

---
+++ footer-twd.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer footer" role="contentinfo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+                        <!-- 					<div class="text-center">
+                                                <i class="fa fa-3x fa-instagram" aria-hidden="true"></i>
+                                            </div> -->
+                        <h1 class="h1-section">Footer/Contacts</h1>
+                    </div>
+                </div>
+            </div>
+		</footer><!-- #colophon -->
+</div><!-- #page -->
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header-twd.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> >
+<div id="page" class="site">
+	<header id="masthead" class="header parallax-window" role="banner" data-parallax="scroll" data-image-src="<?php header_image();?>">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="bg-block-logo">
+                        <h2 class="h2-logo"><?php bloginfo('name');?></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="center-middle">
+            <h1 class="name">Digital Marketing Agency for Hotels</h1>
+            <p class="logo-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium hic nulla, amet consequatur necessitatibus.</p>
+            <a href="#about" class="page-scroll link-down"><i class="fa fa-3x fa-angle-down" aria-hidden="true"></i></a>
+        </div>
+
+	</header><!-- #masthead -->
+        <div id="content" class="site-content">
